abbdd260ad0b5f41f3c4089c20bc855a56252a70
Cheng-Lung Sung <clsung@gmail.com>
2014-10-07 00:56:17 +0800

    update dockerfile

--- docker/build.sh
+++ docker/build.sh
@@ -3,5 +3,6 @@ docker build -t clsung/tailer_build build/
 docker run -d --name tailer_build clsung/tailer_build
 docker cp tailer_build:/gopath/bin/tailer run/
 docker stop tailer_build
+docker rm tailer_build
 docker rmi clsung/tailer_build
 docker build -t clsung/tailer run/
