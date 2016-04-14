bc70021b6e068df7dfb85937ae10397ea9dc1836
Cheng-Lung Sung <clsung@gmail.com>
2016-04-14 15:58:07 +0800

    fix Docker build

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker build -t clsung/tailer_build build/
 docker run -d --name tailer_build clsung/tailer_build
-docker cp tailer_build:/gopath/bin/tailer run/
+docker cp tailer_build:/go/bin/tailer run/
 docker stop tailer_build
 docker rm tailer_build
 docker rmi clsung/tailer_build
